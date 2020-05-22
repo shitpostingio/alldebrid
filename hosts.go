6339f16d9f3e3cac3ad97b2bd637704b110a862f
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2020-05-23 00:21:10 +0200

    add hosts

---
+++ hosts.go
@@ -0,0 +1,45 @@
+package alldebrid
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+//Domains is the domains response struct
+type Domains struct {
+	Status string `json:"status"`
+	Data   struct {
+		Hosts       []string `json:"hosts"`
+		Streams     []string `json:"streams"`
+		Redirectors []string `json:"redirectors"`
+	} `json:"data,omitempty"`
+	Error Error `json:"error,omitempty"`
+}
+
+//GetDomainsOnly returns list of supported hosts domains and redirectors
+func (c *Client) GetDomainsOnly() (Domains, error) {
+	resp, err := http.Get(fmt.Sprintf(hostsdomains, hosts, c.AppName))
+
+	if err != nil {
+		return Domains{}, err
+	}
+
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+
+	var doms Domains
+
+	err = decoder.Decode(&doms)
+	if err != nil {
+		return Domains{}, err
+	}
+
+	if doms.Status != "success" {
+		return Domains{}, errors.New(doms.Error.Message)
+	}
+
+	return doms, nil
+}
