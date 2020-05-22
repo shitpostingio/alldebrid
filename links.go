5cf5b589ed5ba6893172e7c719650d0079a13c5b
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2020-05-22 20:04:06 +0200

    add streaming functions

--- links.go
+++ links.go
@@ -8,7 +8,9 @@ import (
 )
 
 const (
-	unlock = "%s/unlock?agent=%s&apikey=%s&link=%s"
+	unlock    = "%s/unlock?agent=%s&apikey=%s&link=%s"
+	streaming = "%s/streaming?agent=%s&apikey=%s&stream=%s&id=%s"
+	delayed   = "%s/delayed?agent=%s&apikey=%s&id=%s"
 )
 
 //UnlockLinkResponse is the response of the unlock link call
@@ -32,6 +34,31 @@ type UnlockLinkResponse struct {
 			Abr      int     `json:"abr,omitempty"`
 		} `json:"streams"`
 		ID string `json:"id"`
+	} `json:"data,omitempty"`
+	Error Error `json:"error,omitempty"`
+}
+
+//StreamingResponse is the response of the streaming call
+type StreamingResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		Link     string  `json:"link,omitempty"`
+		Filename string  `json:"filename"`
+		Filesize float64 `json:"filesize"`
+		Delayed  int     `json:"delayed,omitempty"`
+	} `json:"data,omitempty"`
+	Error Error `json:"error,omitempty"`
+}
+
+//DelayedResponse is the response of the delayed call
+type DelayedResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		Status   int    `json:"status"`
+		Speed    int    `json:"speed"`
+		TimeLeft int    `json:"time_left"`
+		Progress int    `json:"progress"`
+		Link     string `json:"link"`
 	} `json:"data,omitempty"`
 	Error Error `json:"error,omitempty"`
 }
@@ -60,4 +87,56 @@ func (c *Client) UnlockLink(link string) (UnlockLinkResponse, error) {
 	}
 
 	return unlockResponse, nil
+}
+
+//StreamingLink returns a delayed id or the direct link
+func (c *Client) StreamingLink(stream, id string) (StreamingResponse, error) {
+	resp, err := http.Get(fmt.Sprintf(streaming, magnetURL, c.AppName, c.APIKEY, stream, id))
+
+	if err != nil {
+		return StreamingResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+
+	var streamResponse StreamingResponse
+
+	err = decoder.Decode(&streamResponse)
+	if err != nil {
+		return StreamingResponse{}, err
+	}
+
+	if streamResponse.Status != "success" {
+		return StreamingResponse{}, errors.New(streamResponse.Error.Message)
+	}
+
+	return streamResponse, nil
+}
+
+// DelayedLink returns a downloadable link for the given delayed id
+func (c *Client) DelayedLink(delayedID string) (DelayedResponse, error) {
+	resp, err := http.Get(fmt.Sprintf(delayed, magnetURL, c.AppName, c.APIKEY, delayedID))
+
+	if err != nil {
+		return DelayedResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+
+	var delayedResponse DelayedResponse
+
+	err = decoder.Decode(&delayedResponse)
+	if err != nil {
+		return DelayedResponse{}, err
+	}
+
+	if delayedResponse.Status != "success" {
+		return DelayedResponse{}, errors.New(delayedResponse.Error.Message)
+	}
+
+	return delayedResponse, nil
 }
