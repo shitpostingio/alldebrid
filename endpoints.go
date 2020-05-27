c992c74bbc3c333d1cacc6720e6a3fa1c053110a
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2020-05-27 19:15:23 +0200

    renamed consts.go to endpoints.go

--- endpoints.go
+++ endpoints.go
@@ -2,7 +2,7 @@ package alldebrid
 
 const (
 	// MagnetURL is the endpoint for the magnet API
-	magnetURL = "https://api.alldebrid.com/v4/magnet"
+	magnet = "https://api.alldebrid.com/v4/magnet"
 
 	magnetupload  = "%s/upload?agent=%s&apikey=%s"
 	magnetstatus  = "%s/status?agent=%s&apikey=%s&id=%s"
@@ -10,7 +10,7 @@ const (
 	magnetrestart = "%s/restart?agent=%s&apikey=%s&id=%s"
 
 	// LinksURL is the endpoint for the link API
-	linksURL = "https://api.alldebrid.com/v4/link"
+	links = "https://api.alldebrid.com/v4/link"
 
 	linkunlock    = "%s/unlock?agent=%s&apikey=%s&link=%s"
 	linkstreaming = "%s/streaming?agent=%s&apikey=%s&stream=%s&id=%s"
@@ -28,7 +28,38 @@ const (
 	userinfo = "%s?agent=%s&apikey=%s"
 
 	// Pin is the endpoint for PIN auth API
-	pin      = "https://api.alldebrid.com/v4/pin"
+	pin = "https://api.alldebrid.com/v4/pin"
+
 	pinget   = "%s/get?agent=%s"
 	pincheck = "%s/check?agent=%s&check=%s&pin=%s"
 )
+
+func getMagnetEndpointRegular() string {
+	return magnet
+}
+
+var getMagnetEndpoint = getMagnetEndpointRegular
+
+func getLinksEndpointRegular() string {
+	return links
+}
+
+var getLinksEndpoint = getLinksEndpointRegular
+
+func getHostsEndpointRegular() string {
+	return hosts
+}
+
+var getHostsEndpoint = getHostsEndpointRegular
+
+func getUserEndpointRegular() string {
+	return user
+}
+
+var getUserEndpoint = getUserEndpointRegular
+
+func getPinEndpointRegular() string {
+	return pin
+}
+
+var getPinEndpoint = getPinEndpointRegular
